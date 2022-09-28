a4f24271072f20f89c9d19897214c7c336861e35
faisalahmed0132 <110627120+faisalahmed0132@users.noreply.github.com>
2022-09-28 20:26:07 +0300

    Add files via upload

---
+++ num and math.py
@@ -0,0 +1,22 @@
+
+#num+string 
+from math import floor
+
+
+from math import *
+print (str (5+5) ,"year")
+
+#rounded number
+print (round(5.7) )
+#same num 
+print(floor (5.9))
+#print round without see pount 
+print(ceil (5.9))
+
+#pow 
+print (pow (2,2))
+
+
+print (max (2,9,6,6,4,3,2,90,10))
+print (min (2,9,6,6,4,3,2,90,10))
+
