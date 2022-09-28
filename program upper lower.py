b37d229c1434186f561e6655ccaae6ca9582a811
faisalahmed0132 <110627120+faisalahmed0132@users.noreply.github.com>
2022-09-28 14:43:02 +0300

    Add files via upload

---
+++ program upper lower.py
@@ -0,0 +1,46 @@
+
+
+
+# make first letter upper for all Sentences
+name="faisal ahmed faisal"
+print (name.title())
+
+
+## make first letter upper for first  Sentences
+name="faisal ahmed faisal"
+print (name.capitalize())
+#///////////////////////////////////////////
+
+#print all upper 
+name="faisal ahmed faisal"
+print (name.upper())
+
+#question about upper
+name="faisal ahmed faisal"
+print (name.isupper())
+
+#question about upper
+name="FAISAL AHMED FAISAL"
+print (name.isupper())
+#///////////////////////////////////////
+
+#print all lower 
+name="FAISAL AHMED FAISAL"
+print (name.lower())
+
+#question about upper
+name="faisal ahmed faisal"
+print (name.isupper())
+
+#question about upper
+name="faisal ahmed faisal"
+print (name.islower())
+
+
+
+
+
+
+
+
+
